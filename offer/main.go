ab560ca055115585bfecf6dd17f46fed4cf614ef
Gaurav Chaware <gaurav.chaware@gmail.com>
2020-06-27 09:42:23 +0530

    updated bundles config for offer service and run opa as sidecar

--- offer/main.go
+++ offer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -8,9 +9,28 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+type opaResult struct {
+	Allow bool `json:"allow"`
+}
+type opaResponse struct {
+	DecisionID string    `json:"decision_id"`
+	Result     opaResult `json: result`
+}
+
+type inputJSONData struct {
+	Method string `json:"method"`
+	API    string `json:"api"`
+	Jwt    string `json:"jwt"`
+}
+
+type opaRequest struct {
+	Input inputJSONData `json:"input"`
+}
 
 type offer struct {
 	OfferID  string `json:"offerid"`
@@ -33,59 +53,84 @@ var offers = allOffers{
 }
 
 func createOffer(w http.ResponseWriter, r *http.Request) {
-	var newOffer offer
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Invalid Format")
-	}
-	json.Unmarshal(reqBody, &newOffer)
-	newOffer.OfferID = strconv.Itoa(rangeIn(1000, 10000))
-	offers = append(offers, newOffer)
-	w.WriteHeader(http.StatusCreated)
+	var allow = authorize(r)
+	if allow {
+		var newOffer offer
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid Format")
+		}
+		json.Unmarshal(reqBody, &newOffer)
+		newOffer.OfferID = strconv.Itoa(rangeIn(1000, 10000))
+		offers = append(offers, newOffer)
+		w.WriteHeader(http.StatusCreated)
 
-	json.NewEncoder(w).Encode(newOffer)
+		json.NewEncoder(w).Encode(newOffer)
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 func getAllOffers(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(offers)
+	var allow = authorize(r)
+	if allow {
+		json.NewEncoder(w).Encode(offers)
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 
 func getOffer(w http.ResponseWriter, r *http.Request) {
-	offerID := mux.Vars(r)["id"]
+	var allow = authorize(r)
+	if allow {
+		offerID := mux.Vars(r)["id"]
 
-	for _, singleOffer := range offers {
-		if singleOffer.OfferID == offerID {
-			json.NewEncoder(w).Encode(singleOffer)
+		for _, singleOffer := range offers {
+			if singleOffer.OfferID == offerID {
+				json.NewEncoder(w).Encode(singleOffer)
+			}
 		}
+	} else {
+		w.WriteHeader(http.StatusForbidden)
 	}
 }
 
 func updateOffer(w http.ResponseWriter, r *http.Request) {
-	offerID := mux.Vars(r)["id"]
-	var updatedOffer offer
+	var allow = authorize(r)
+	if allow {
+		offerID := mux.Vars(r)["id"]
+		var updatedOffer offer
 
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "Invalid Format")
-	}
-	json.Unmarshal(reqBody, &updatedOffer)
-
-	for i, singleOffer := range offers {
-		if singleOffer.OfferID == offerID {
-			singleOffer.Title = updatedOffer.Title
-			singleOffer.Comments = updatedOffer.Comments
-			offers = append(offers[:i], singleOffer)
-			json.NewEncoder(w).Encode(singleOffer)
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid Format")
 		}
+		json.Unmarshal(reqBody, &updatedOffer)
+
+		for i, singleOffer := range offers {
+			if singleOffer.OfferID == offerID {
+				singleOffer.Title = updatedOffer.Title
+				singleOffer.Comments = updatedOffer.Comments
+				offers = append(offers[:i], singleOffer)
+				json.NewEncoder(w).Encode(singleOffer)
+			}
+		}
+	} else {
+		w.WriteHeader(http.StatusForbidden)
 	}
 }
 
 func deleteOffer(w http.ResponseWriter, r *http.Request) {
-	offerID := mux.Vars(r)["id"]
-	offerIndex := getIndex(offers, offerID)
-	updatedList := make([]offer, 0)
-	updatedList = append(updatedList, offers[:offerIndex]...)
-	updatedList = append(updatedList, offers[offerIndex+1:]...)
-	offers = updatedList
+	var allow = authorize(r)
+	if allow {
+		offerID := mux.Vars(r)["id"]
+		offerIndex := getIndex(offers, offerID)
+		updatedList := make([]offer, 0)
+		updatedList = append(updatedList, offers[:offerIndex]...)
+		updatedList = append(updatedList, offers[offerIndex+1:]...)
+		offers = updatedList
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+	}
 }
 
 func getIndex(offers []offer, offerID string) int {
@@ -99,6 +144,33 @@ func getIndex(offers []offer, offerID string) int {
 
 func rangeIn(low, hi int) int {
 	return low + rand.Intn(hi-low)
+}
+
+func authorize(r *http.Request) bool {
+	var authHeader = r.Header.Get("Authorization")
+	var apiEndPoint = r.URL.Path
+	var reqMethod = r.Method
+	var jwt = strings.Split(authHeader, "Bearer ")[1]
+
+	varInputJSONData := &inputJSONData{API: apiEndPoint, Method: reqMethod, Jwt: jwt}
+	varOpaRequest := &opaRequest{Input: *varInputJSONData}
+	jsonValue, _ := json.Marshal(varOpaRequest)
+	fmt.Println(string(jsonValue))
+	response, err := http.Post("http://localhost:8181/v1/data/httpapi/authz", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Printf("OPA request failed with error %s\n", err)
+		return false
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+
+		var res = new(opaResponse)
+		err = json.Unmarshal(data, &res)
+		if err != nil {
+			fmt.Println("Error unmarshalling OPA response")
+		}
+		return res.Result.Allow
+	}
 }
 
 func main() {
