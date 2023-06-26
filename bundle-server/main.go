36c3aa4b757701c24404cc9324037394bf8bc3bf
Gaurav <gaurav.chaware@gmail.com>
2023-06-26 18:48:26 +0530

    Update main.go

--- bundle-server/main.go
+++ bundle-server/main.go
@@ -20,7 +20,29 @@ func main() {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
 
+	printPrimeNumbers(5,19)
 	// start HTTP server with `http.DefaultServeMux` handler
 	log.Fatal(http.ListenAndServe(":9000", nil))
 
+}
+
+func printPrimeNumbers(num1, num2 int){
+   if num1<2 || num2<2{
+      fmt.Println("Numbers must be greater than 2 for this to work.")
+      return
+   }
+   for num1 <= num2 {
+      isPrime := true
+      for i:=2; i<=int(math.Sqrt(float64(num1))); i++{
+         if num1 % i == 0{
+            isPrime = false
+            break
+         }
+      }
+      if isPrime {
+         fmt.Printf("Found Prime number: %d ", num1)
+      }
+      num1++
+   }
+   fmt.Println()
 }
